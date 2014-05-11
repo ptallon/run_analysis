4565107e0789284df64635dffddb73ba100545b9
Paul Tallon <pptallon@loyola.edu>
2014-05-11 03:33:59 -0400

    Commit All Plot Files

---
+++ plot3.R
@@ -0,0 +1,34 @@
+plot3 <- function(){
+    ## This is the first project for Exploratory Data Analysis - plot 3 of 4
+    filename = "household_power_consumption.txt"
+    ##tab5rows <- read.table(filename, header = TRUE, sep=";", na.strings="?",nrows = 10)
+    ##classes <- sapply(tab5rows, class)
+
+    f <- file(filename,"rt");
+    
+    nolines <- 100
+    greped<-c()
+    repeat {
+        lines=readLines(f,n=nolines)       #read lines
+        idx <- grep("^[12]/2/2007", lines) #find those that match
+        greped<-c(greped, lines[idx])      #add the found lines
+        #
+        if(nolines!=length(lines)) {
+            break #are we at the end of the file?
+        }
+    }
+    close(f)
+    
+    tc<-textConnection(greped,"rt") #now we create a text connection and load data
+    myData <- read.csv(tc,sep=";",header=FALSE, na.strings="?")
+    names(myData) <- c("Date", "Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+    myData$Timing <- paste(myData$Date, sep=" ", myData$Time)
+    myData$Timing <- strptime(myData$Timing, "%d/%m/%Y %H:%M:%S")    
+    
+    png(filename="plot3.png", width=480, height=480)
+    plot(myData$Timing,myData$Sub_metering_1,xlab="",ylab="Energy Sub Metering",type="l",main=NULL,col="black")
+    lines(myData$Timing,myData$Sub_metering_2, type = "l", col = "red")
+    lines(myData$Timing,myData$Sub_metering_3, type = "l", col = "blue")
+    legend("topright", col=c("black", "blue", "red"), cex = 0.95, lty=1,legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    dev.off()
+}
